2d40d451803fdc42b7365c5901685f9486b5b2ac
karthikeyan <keyan156@gmail.com>
2024-02-25 12:39:49 +0000

    new changes

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, Form, UploadFile
 import librosa
 import asyncio
 import os
@@ -6,7 +6,7 @@ import os
 app = FastAPI()
 
 @app.post("/process-audio/")
-async def process_audio(file: UploadFile = File(...)):
+async def process_audio(file: UploadFile = File(...), description: str = Form(...)):
     # Save temporary audio file
     temp_file = f"temp_{file.filename}"
     with open(temp_file, 'wb+') as f:
@@ -18,12 +18,11 @@ async def process_audio(file: UploadFile = File(...)):
     # Remove temporary file
     os.remove(temp_file)
     
-    return {"filename": file.filename, "duration": duration}
+    return {"filename": file.filename, "duration": duration, "lib":description}
 
 async def get_audio_duration(file_path: str) -> float:
     # Running librosa in a threadpool to prevent blocking
     loop = asyncio.get_event_loop()
     audio_signal, sample_rate = await loop.run_in_executor(None, librosa.load, file_path)
     duration = librosa.get_duration(y=audio_signal, sr=sample_rate)
-    print(duration)
     return duration
